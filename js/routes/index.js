c96f9522ac76f3a2cb0c721f548bfb711ab21d69
James Peterson <jpeterson903@yahoo.com>
2017-07-27 11:39:28 -0700

    Remove unused code from routes and some reformatting

--- js/routes/index.js
+++ js/routes/index.js
@@ -1,44 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-// const passportLinkedIn = require('../auth/linkedin')
 const passportGithub = require('../auth/github')
 const passportTwitter = require('../auth/twitter')
 const passportGoogle = require('../auth/google')
 
 router.ensureAuthenticated = function (req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
+  if (req.isAuthenticated()) { return next() }
   req.session.returnTo = req.path
   res.redirect('/login')
 }
 
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' })
-// })
-
-// router.get('/login', function(req, res, next) {
-//   res.send('Go back and register!')
-// })
-
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+router.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/')
 })
-
-// router.get('/auth/linkedin', passportLinkedIn.authenticate('linkedin'))
-
-// router.get('/auth/linkedin/callback',
-//   passportLinkedIn.authenticate('linkedin', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     // Successful authentication
-//     res.json(req.user)
-//   })
 
 router.get('/auth/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }))
 
 router.get('/auth/github/callback',
   passportGithub.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
     res.redirect(req.session.returnTo || '/')
     delete req.session.returnTo
   })
@@ -47,7 +29,7 @@ router.get('/auth/twitter', passportTwitter.authenticate('twitter'))
 
 router.get('/auth/twitter/callback',
   passportTwitter.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
     res.redirect(req.session.returnTo || '/')
     delete req.session.returnTo
   })
@@ -56,7 +38,7 @@ router.get('/auth/google', passportGoogle.authenticate('google', { scope: ['prof
 
 router.get('/auth/google/callback',
   passportGoogle.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
     res.redirect(req.session.returnTo || '/')
     delete req.session.returnTo
   })
