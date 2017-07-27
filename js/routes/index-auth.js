903436b96f88bef477981cfc18e259c45a0e5b3f
James Peterson <jpeterson903@yahoo.com>
2017-07-27 13:32:29 -0700

    Create router that handles authentication

---
+++ js/routes/index-auth.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const authRouter = express.Router()
+
+const passportGithub = require('../auth/github')
+const passportTwitter = require('../auth/twitter')
+const passportGoogle = require('../auth/google')
+
+authRouter.ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) { return next() }
+  req.session.returnTo = req.path
+  res.redirect('/login')
+}
+
+authRouter.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+authRouter.get('/auth/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }))
+
+authRouter.get('/auth/github/callback',
+  passportGithub.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect(req.session.returnTo || '/')
+    delete req.session.returnTo
+  })
+
+authRouter.get('/auth/twitter', passportTwitter.authenticate('twitter'))
+
+authRouter.get('/auth/twitter/callback',
+  passportTwitter.authenticate('twitter', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect(req.session.returnTo || '/')
+    delete req.session.returnTo
+  })
+
+authRouter.get('/auth/google', passportGoogle.authenticate('google', { scope: ['profile'] }))
+
+authRouter.get('/auth/google/callback',
+  passportGoogle.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect(req.session.returnTo || '/')
+    delete req.session.returnTo
+  })
+
+module.exports = authRouter
