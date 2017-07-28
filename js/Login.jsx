78ddfece7378eb67dad4dfb00e7e5334472a3374
James Peterson <jpeterson903@yahoo.com>
2017-07-28 09:44:46 -0700

    Center authentication images and links

--- js/Login.jsx
+++ js/Login.jsx
@@ -4,20 +4,26 @@ const Login = (props) => {
   return (
     <div className='login-wrapper'>
       <div className='auth-link'>
-        <a className='auth-github' href='/auth/github'>
+        <a href='/auth/github'>
           <img className='github-mark' src='/public/icons/GitHub-Mark.png' alt='Github' />
+        </a>
+        <a className='auth-github' href='/auth/github'>
           Login with GitHub
         </a>
       </div>
       <div className='auth-link'>
         <a className='auth-twitter' href='/auth/twitter'>
           <img className='twitter-mark' src='/public/icons/twitter-icon.png' alt='Twitter' />
+        </a>
+        <a className='auth-twitter' href='/auth/twitter'>
           Login with Twitter
         </a>
       </div>
       <div className='auth-link'>
         <a className='auth-google' href='/auth/google'>
           <img className='google-mark' src='/public/icons/google-mark.png' alt='Google' />
+        </a>
+        <a className='auth-google' href='/auth/google'>
           Login with Google
         </a>
       </div>
