678d6f214e031fff6647608f319e4252da3d848d
James Peterson <jpeterson903@yahoo.com>
2017-08-15 15:43:26 -0700

    Add status code for post requests

--- js/routes/index.js
+++ js/routes/index.js
@@ -73,7 +73,7 @@ router.post('/create-poll', authRouter.ensureAuthenticated, (req, res) => {
 
     user.save((err, user) => {
       if (err) return console.error(err)
-      res.json(newPoll)
+      res.status(201).json(newPoll)
     })
   })
   .catch(err => {
