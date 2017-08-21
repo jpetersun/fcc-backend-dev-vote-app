fe3bd593b2ca65635bcd3b135188225d0fc73870
James Peterson <jpeterson903@yahoo.com>
2017-08-21 14:13:41 -0700

    Remove commented console.logs and add white space for readability

--- js/routes/index.js
+++ js/routes/index.js
@@ -10,6 +10,7 @@ import randomColor from 'random-color'
 
 router.get('/account-details', authRouter.ensureAuthenticated, (req, res) => {
   const theUser = User.findOne({ someID: req.user.someID })
+
   theUser.then((user) => {
     res.json(user)
   })
@@ -18,9 +19,9 @@ router.get('/account-details', authRouter.ensureAuthenticated, (req, res) => {
   })
 })
 
-// console.log(allPolls.then)
 router.get('/polls', (req, res) => {
   const users = User.find({})
+
   users.then((user) => {
     res.send(user)
   })
@@ -31,12 +32,12 @@ router.get('/polls', (req, res) => {
 
 router.get('/polls/:userId/:id', (req, res) => {
   const theUser = User.findOne({ _id: req.params.userId })
+
   theUser.then((user) => {
     const polls = user.polls
     const thePoll = polls.filter((item) => {
       return String(item._id) === req.params.id
     })
-    // console.log(thePoll)
     res.send(thePoll)
   })
   .catch(err => {
@@ -54,9 +55,8 @@ router.get('/account', authRouter.ensureAuthenticated, (req, res, next) => {
 })
 
 router.post('/create-poll', authRouter.ensureAuthenticated, (req, res) => {
-  // console.log(req.body)
-  // console.log(req.body.user._id)
   let id
+
   if (req.body.user) {
     id = req.body.user._id
   } else {
@@ -64,6 +64,7 @@ router.post('/create-poll', authRouter.ensureAuthenticated, (req, res) => {
   }
 
   const theUser = User.findOne({_id: id})
+
   theUser.then((user) => {
     const options = req.body.values.map(value => {
       return {
@@ -91,19 +92,17 @@ router.post('/create-poll', authRouter.ensureAuthenticated, (req, res) => {
 })
 
 router.put('/poll-results/:userId/:id', (req, res) => {
-  // console.log(req.params.id)
-  // console.log(req.body)
-
   User.findById(req.params.userId, (err, user) => {
     if (err) console.error(err)
+
     const polls = user.polls
     const thePoll = polls.filter((item) => {
       return String(item._id) === req.params.id
     }).pop()
-    // console.log(thePoll[0].options)
-    // const option = thePoll[0].options.id(req.body._id)
+
     const option = _.find(thePoll.options, {name: req.body.name})
     const poll = thePoll
+
     if (poll.votersIpAddress.indexOf(ip.address()) >= 0) {
       res.send('Already voted')
     } else {
@@ -117,13 +116,16 @@ router.put('/poll-results/:userId/:id', (req, res) => {
 
 router.delete('/user-poll/:userId/:id', (req, res) => {
   const theUser = User.findOne({ _id: req.params.userId })
+
   theUser.then((user) => {
     const polls = user.polls
     const filteredPolls = polls.filter((item) => {
       return String(item._id) !== req.params.id
     })
+
     user.polls = filteredPolls
     user.save()
+
     res.json(user.polls)
   })
   .catch(err => {
